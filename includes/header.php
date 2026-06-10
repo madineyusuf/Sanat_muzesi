ace0a591bf20d5e4f61b15b196b4037691a1183d
Madina Yusupova <yusvpova@gmail.com>
2026-06-10 15:04:58 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -7,23 +7,23 @@
     <title>Sanat Müzesi</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="/~st24360859922/assets/css/style.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand" href="/index.php"> Sanat Müzesi</a>
+        <a class="navbar-brand" href="/~st24360859922/index.php">🎨 Sanat Müzesi</a>
         <div class="ms-auto d-flex align-items-center gap-2">
             <?php if (isLoggedIn()): ?>
-                <a href="/Sanat_muzesi-main/profile.php" class="btn btn-outline-light btn-sm">
+                <a href="/~st24360859922/profile.php" class="btn btn-outline-light btn-sm">
                     <i class="bi bi-person-circle"></i> <?= htmlspecialchars($_SESSION['kullanici_adi']) ?>
                 </a>
-                <a href="/Sanat_muzesi-main/logout.php" class="btn btn-outline-danger btn-sm">Çıkış</a>
+                <a href="/~st24360859922/logout.php" class="btn btn-outline-danger btn-sm">Çıkış</a>
             <?php else: ?>
-                <a href="/Sanat_muzesi-main/login.php" class="btn btn-outline-light btn-sm">Giriş</a>
-                <a href="/Sanat_muzesi-main/register.php" class="btn btn-light btn-sm">Kayıt Ol</a>
+                <a href="/~st24360859922/login.php" class="btn btn-outline-light btn-sm">Giriş</a>
+                <a href="/~st24360859922/register.php" class="btn btn-light btn-sm">Kayıt Ol</a>
             <?php endif; ?>
         </div>
     </div>
 </nav>
-
     
