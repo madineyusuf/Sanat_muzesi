9ed39b2209601bb435a619fc3a0c9b3a1abe9e0e
Madina Yusupova <yusvpova@gmail.com>
2026-06-10 21:11:37 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sanat Müzesi</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="/~st24360859922/assets/css/style.css">
+    <link class="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link class="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css">
+    <link class="stylesheet" href="/~st24360859922/assets/css/style.css">
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="/~st24360859922/index.php">Sanat Müzesi</a>
@@ -26,4 +27,3 @@
         </div>
     </div>
 </nav>
-    
