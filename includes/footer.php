cd85066dbf2907ba4e2ba4926d8d2986834a447e
Madina Yusupova <yusvpova@gmail.com>
2026-06-10 20:21:59 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-dark text-light text-center py-3 fixed-bottom">
+<footer class="bg-dark text-light text-center py-3">
     <small>© BM BTÜ 2026 </small>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
